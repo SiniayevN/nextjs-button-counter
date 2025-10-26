d5a1ccece2830d1679ea47200e0db39be29ce464
Nikita Siniayev <ns21@usf.edu>
2025-10-26 15:56:46 -0400

    page tsx new

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ export default function Page() {
   return (
     <main className="main-layout">
       <h1 className="page-title">Next.js Counter</h1>
+
       <Counter />
+      <Counter initialCount={5} initialStep={2} />
     </main>
   );
 }
